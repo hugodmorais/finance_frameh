802ab5cdcf57ca86b41997b902407472933e197b
Hugo Morais <hugodanielmorais@gmail.com>
2019-05-11 16:44:52 +0100

    Exceptions

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -7,8 +7,12 @@ class Stock < ActiveRecord::Base
     end
     
     def self.new_from_lookup(ticker_symbol)
-        looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-        new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+        begin
+            looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+            new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+        rescue Exception => e
+            return nil
+        end
     end
     
     def self.strip_commas(number)
