ce0edd4728c02788851051c33f839286d06350fc
Hugo Morais <hugodanielmorais@gmail.com>
2019-05-11 19:14:28 +0100

    minor fix AJAX

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,23 +1,12 @@
 class StocksController < ApplicationController
   
     def search
-      if params[:stock].present?
-        @stock = Stock.new_from_lookup(params[:stock])
-        if @stock
-          respond_to do |format|
-            format.js { render partial: 'users/result' }
-          end
+        if params[:stock].blank?
+          flash.now[:danger] = "You have entered an empty search string"
         else
-          respond_to do |format|
-            flash.now[:danger] = "You have entered an incorrect symbol"
-            format.js { render partial: 'users/result' }
-          end
+          @stock = Stock.new_from_lookup(params[:stock])
+          flash.now[:danger] = "You have entered an incorrect symbol" unless @stock
         end
-      else
-        respond_to do |format|
-          flash.now[:danger] = "You have entered an empty string"
-          format.js { render partial: 'users/result' }
-        end
-      end
+        render partial: 'users/result'
     end
 end
