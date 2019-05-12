b4af046b087d1c624ff733bdcb574648098f5085
Hugo Morais <hugodanielmorais@gmail.com>
2019-05-12 14:34:37 +0100

    Friendship relation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create([{ 
+    first_name: 'Kenia',
+    last_name: 'Soares'  ,
+    email: 'kenia@gmail.com',
+    password: '123456',
+    password_confirmation: '123456'
+}]),
+user1 = User.create([{ 
+    first_name: 'Hugo',
+    last_name: 'Morais'  ,
+    email: 'hugo@gmail.com',
+    password: '123456',
+    password_confirmation: '123456'
+}]),
+user2 = User.create([{ 
+    first_name: 'Dani',
+    last_name: 'Soares'  ,
+    email: 'dani@gmail.com',
+    password: '123456',
+    password_confirmation: '123456'
+}]),
+user3 = User.create([{ 
+    first_name: 'freitas',
+    last_name: 'Soares'  ,
+    email: 'freitas@gmail.com',
+    password: '123456',
+    password_confirmation: '123456'
+}]),
+user4 = User.create([{ 
+    first_name: 'Hugo',
+    last_name: 'Soares'  ,
+    email: 'pucklis@gmail.com',
+    password: '123456',
+    password_confirmation: '123456'
+}]),
+user5 = User.create([{ 
+    first_name: 'Keny',
+    last_name: 'Soares'  ,
+    email: 'keny@gmail.com',
+    password: '123456',
+    password_confirmation: '123456'
+}])
